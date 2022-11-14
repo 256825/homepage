25e53b399aec7dba735a271e22a5bf42a59342cf
256825 <98037580+256825@users.noreply.github.com>
2022-11-14 13:30:13 -0600

    nov 14th

--- myScripts.js
+++ myScripts.js
@@ -14,4 +14,62 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
    }
-   
+ 
+
+   //JACKIE'S NOTES
+if (localStorage["monday"]) {
+    var monday = localStorage["monday"];
+    document.getElementById("monday").value = monday;
+  } 
+  function saveMonday() {
+    var monday = document.getElementById("monday").value;
+    localStorage.setItem("monday", monday);
+  }
+  
+  if (localStorage["tuesday"]) {
+    var tuesday = localStorage["tuesday"];
+    document.getElementById("tuesday").value = tuesday;
+  }
+  function saveTuesday() {
+    var tuesday = document.getElementById("tuesday").value;
+    localStorage.setItem("tuesday", tuesday);
+  }
+  
+  if (localStorage["wednesday"]) {
+    var wednesday = localStorage["wednesday"];
+    document.getElementById("wednesday").value = wednesday;
+  }
+  function saveWednesday() {
+    var wednesday = document.getElementById("wednesday").value;
+    localStorage.setItem("wednesday", wednesday);
+  }
+  
+  if (localStorage["thursday"]) {
+    var thursday = localStorage["thursday"];
+    document.getElementById("thursday").value = thursday;
+  }
+  function saveThursday() {
+    var thursday = document.getElementById("thursday").value;
+    localStorage.setItem("thursday", thursday);
+  }
+  
+  if (localStorage["friday"]) {
+    var friday = localStorage["friday"];
+    document.getElementById("friday").value = friday;
+  }
+  function saveFriday() {
+    var friday = document.getElementById("friday").value;
+    localStorage.setItem("friday", friday);
+  }
+  
+  
+  var show = false;
+  function showNote() {
+    if (show == false) {
+      document.getElementById("notes").style.opacity = '1';
+      show = true;
+    } else if (show == true) {
+      document.getElementById("notes").style.opacity = '0';
+      show = false;
+    }
+  }
